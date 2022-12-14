79177ddede58b2cce0951e8facdea5a8f7c1b320
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2022-12-14 23:20:26 +0530

    feat: added tailwind css and added configuration

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,6 @@
   import NavigationBar from "./lib/NavigationBar.svelte";
 </script>
 
-<h1>Hello world</h1>
+
 
 <NavigationBar />
--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -3,15 +3,3 @@
 </nav>
 
 
-<style scoped>
-
-    #navigation-bar{
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100vh;
-        width: 30vh;
-        float: right;
-    }
-
-</style>
