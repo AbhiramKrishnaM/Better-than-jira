b078f40d30e76c0c798937680da4c224ee4032dc
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2022-12-15 22:39:17 +0530

    feat: added avatar and user name and designation

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -1,4 +1,17 @@
-<nav class="w-1/5 h-screen bg-slate-100/40 border-r border-slate-200">
+<nav class="w-1/5 h-screen bg-slate-100/40 border-r border-slate-200 px-8 py-10 flex justify-center">
+
+
+    <div id="user-info" class="w-full h-16  flex  items-center justify-between">
+
+        <div id="avatar-image" class="h-16 w-16 rounded-full drop-shadow-xl bg-contain bg-[url(@/static/man.png)]"></div>
+
+       
+        <div id="info " class="text-slate-900/70">
+            <h1 class="text-lg font-bold leading-none ">Abhiram Krishna</h1>
+            <span class="text-sm">Full Stack Engineer</span>
+        </div>
+
+    </div>
 
     
 </nav>
