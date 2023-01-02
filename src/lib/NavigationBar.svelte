8d8992207605d42782a975703b55912325c8b2bd
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-01-02 21:32:38 +0530

    feat: navigation bar updated with new tabs

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -1,19 +1,27 @@
-<nav class="w-1/5 h-screen bg-slate-100/40 border-r border-slate-200 px-8 py-10 flex justify-center">
+<nav class="w-1/5 h-screen bg-slate-100/40 border-r border-slate-200  py-10 flex flex-col">
 
-
-    <div id="user-info" class="w-full h-16  flex  items-center justify-between">
-
+    <div id="user-info" class="w-full h-16 px-8 flex items-center justify-between">
         <div id="avatar-image" class="h-16 w-16 rounded-full drop-shadow-xl bg-contain bg-[url(@/static/man.png)]"></div>
-
-       
         <div id="info " class="text-slate-900/70">
             <h1 class="text-lg font-bold leading-none ">Abhiram Krishna</h1>
             <span class="text-sm">Full Stack Engineer</span>
         </div>
-
     </div>
 
-    
+    <div class="mt-8">
+        <h6 class="text-slate-900/70 px-8 text-base font-medium">Menu</h6>
+
+        <ul>
+            <li class="py-2 px-8  cursor-pointer text-gray-500/70">My Tasks</li>
+            <li class="py-2 px-8  cursor-pointer text-gray-500/70">Inbox</li>
+            <li class="py-2 px-8  cursor-pointer text-gray-500/70">Projects</li>
+            <li class="py-2 px-8  cursor-pointer text-gray-500/70">Standups</li>
+            <li class="py-2 px-8  cursor-pointer text-gray-500/70">Meetings</li>
+            <li class="py-2 px-8  cursor-pointer text-gray-500/70">Settings</li>
+
+        </ul>
+    </div>
+   
 </nav>
 
 
